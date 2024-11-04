4bddbdf6372aadff6b4cea6d9e0003c559f67c2e
amirhamjacse <amirhamza824@gmail.com>
2024-11-04 16:12:34 +0600

    bigger number in theree number

---
+++ big_number_two.py
@@ -0,0 +1,10 @@
+num_one = int(input("Enter first number: "))
+num_two = int(input("Enter Second number: "))
+num_three = int(input("Enter Third number: "))
+
+if num_one > num_two and num_one > num_three:
+    print("Number one is grater")
+elif num_two > num_three:
+    print("Number two is grater")
+else:
+    print("Number theree is grater")
