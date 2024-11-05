bc2f201b88fe541077886c0d072d8bb55469437f
amirhamjacse <amirhamza824@gmail.com>
2024-11-05 11:11:27 +0600

    added great with username

---
+++ great_with_username.py
@@ -0,0 +1,7 @@
+#Write a program that asks the user for their name and greets them with their name.
+#Modify the previous program such that only the users Alice and Bob are greeted with their names.
+
+username = str(input("Enter your name: "))
+
+if username ==  "Alice" or username == "Bob":
+    print("Good morning", username)
