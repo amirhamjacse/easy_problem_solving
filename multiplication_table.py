156c9110b01c1ea45a76fb95ed5c122ff9257c4a
amirhamjacse <amirhamza824@gmail.com>
2024-11-05 13:01:00 +0600

    added multiple table

---
+++ multiplication_table.py
@@ -0,0 +1,6 @@
+#Write a program that prints a multiplication table for numbers up to 12.
+
+for i in range(1, 13):
+    for j in range(1, 11):
+        print(i, "x", j, "=", i*j)
+        # print(i*j)
